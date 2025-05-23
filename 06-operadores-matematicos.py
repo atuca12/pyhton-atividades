7bace63c5cb0521e6bd32da05c8a68770419be56
atuca12 <nascimentosantosarthur@gmail.com>
2025-05-23 10:04:37 -0300

    Update 06-operadores-matematicos.py

--- 06-operadores-matematicos.py
+++ 06-operadores-matematicos.py
@@ -7,3 +7,8 @@ print(a + b, a - b, a * b, a / b, a ** b, a // b, a % b)
 Crie quatro novas variaveis de numeros inteiros, some elas, 
 calcule a média e exiba o resultado na tela
 """
+c = 1
+d = 2
+e = 3
+f = 4
+print((c + d + e + f)/4,)
